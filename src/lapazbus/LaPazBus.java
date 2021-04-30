598e15077cae7b96e80d0683768ea80d8d353529
Tostadora <Tostadora@TostadoraV>
2021-04-30 19:05:04 +0100

    agregue sout

--- src/lapazbus/LaPazBus.java
+++ src/lapazbus/LaPazBus.java
@@ -16,6 +16,7 @@ public class LaPazBus {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        System.out.println("Hola mundo");
     }
     
 }
