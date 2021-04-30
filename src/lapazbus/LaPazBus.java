54e374683fa88f581dedef848b18661727b7dcb5
Tostadora <Tostadora@TostadoraV>
2021-04-30 20:00:42 +0100

    Interesanteaa

--- src/lapazbus/LaPazBus.java
+++ src/lapazbus/LaPazBus.java
@@ -17,6 +17,7 @@ public class LaPazBus {
     public static void main(String[] args) {
         // TODO code application logic here
         System.out.println("Hola mundo");
+        System.out.println("aaaa");
     }
     
 }
