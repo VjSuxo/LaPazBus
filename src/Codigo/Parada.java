500c9f548bc0be8552c4bd1acb8febf35a6ba2c9
Tostadora <Tostadora@TostadoraV>
2021-05-09 09:41:40 +0100

    Creacion y manipulacion de vectores terminado, modificacion tarifa se volvio un vector y parada tambien se volvio un vector en el uml igual esta ya modificado

--- src/Codigo/Parada.java
+++ src/Codigo/Parada.java
@@ -11,27 +11,28 @@ package Codigo;
  */
 public class Parada {
     
-    private String[] Zona;
-    private String[] Calle;
-
-    public Parada(String[] Zona, String[] Calle) {
+    private String Zona;
+    private String Calle;
+    private int indice,tp;
+    public Parada(String Zona, String Calle) {
         this.Zona = Zona;
+        
         this.Calle = Calle;
     }
 
-    public String[] getZona() {
+    public String getZona() {
         return Zona;
     }
 
-    public void setZona(String[] Zona) {
+    public void setZona(String Zona) {
         this.Zona = Zona;
     }
 
-    public String[] getCalle() {
+    public String getCalle() {
         return Calle;
     }
 
-    public void setCalle(String[] Calle) {
+    public void setCalle(String Calle) {
         this.Calle = Calle;
     }
     
